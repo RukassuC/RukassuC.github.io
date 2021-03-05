6423a22c4d6cac3b189d6ee6fbe274944b932e41
RukassuC <79521244+RukassuC@users.noreply.github.com>
2021-03-05 20:20:50 -0300

    Acrescentando JavaScript

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,48 @@
+ /******
+ Esse código tá horrível, 
+ buscar soluções mais eficientes, prfv.
+*******/
+ 
+ const bred = document.querySelector('#b-red')
+ const bblue = document.querySelector('#b-blue')
+ const bgreen = document.querySelector('#b-green')
+
+
+function changeBack1(){
+    const body = document.querySelector('body')
+    if(body.style.backgroundColor == 'red'){
+        body.style.backgroundColor = 'white'
+        bred.style.backgroundColor = 'red'
+    } else {
+        body.style.backgroundColor = 'red'
+        bred.style.backgroundColor = 'white'
+    }
+}
+function changeBack2(){
+    const body = document.querySelector('body')
+    if(body.style.backgroundColor == 'royalblue'){
+        body.style.backgroundColor = 'white'
+        bblue.style.backgroundColor = 'royalblue'
+    } else {
+        body.style.backgroundColor = 'royalblue'
+        bblue.style.backgroundColor = 'white'
+    }
+}
+
+function changeBack3(){
+    const body = document.querySelector('body')
+    if(body.style.backgroundColor == 'seagreen'){
+        body.style.backgroundColor = 'white'
+        bgreen.style.backgroundColor = 'seagreen'
+    } else {
+        body.style.backgroundColor = 'seagreen'
+        bgreen.style.backgroundColor = 'white'
+    }
+}
+
+
+bred.addEventListener('click',changeBack1)
+bblue.addEventListener('click',changeBack2)
+bgreen.addEventListener('click',changeBack3)
+
 
